6eb6d0819f43f18e6ed18ba73818829c644e3f0e
Mahmoud_alqiaty <mahmoudalqiaty15@gmail.com>
2021-08-07 18:37:52 +0200

    first commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,15 +9,16 @@ const Home = () => {
 
   const headers = 
   {
+    "Authorization": 
+    "Bearer 5954192-Lr0ma2vvgF34aPWMmAibWlam2YxVTuWb2CPqY7MVTr",
     'Content-Type' : 'application/x-www-form-urlencoded; application/json',
     'Access-Control-Allow-Origin' : '*',
   }
-
+const key = `7cf9d5d72c3f9e916b1516a56477a9bd`
   useEffect(() => {
     axios({
       method: "GET",
-      url: "https://api.covidtracking.com/v1/status.json",
-      headers: headers
+      url: `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${key}`,
     })
     .then(res=>{
       res = res
